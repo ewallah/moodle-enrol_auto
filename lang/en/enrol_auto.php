b785d77b1889ae60987a7d3a73b5f821aea51efa
Renaat Debleu <rdebleu@eWallah.net>
2024-04-05 19:24:11 +0000

    code review

--- lang/en/enrol_auto.php
+++ lang/en/enrol_auto.php
@@ -22,6 +22,11 @@
  * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
+$string['auto:config'] = 'Configure auto enrol instances';
+$string['auto:enrolself'] = 'Enrol self in the course';
+$string['auto:manage'] = 'Manage enrolled users';
+$string['auto:unenrol'] = 'Unenrol users from course';
+$string['auto:unenrolself'] = 'Unenrol self from the course';
 $string['defaultrole'] = 'Default role assignment';
 $string['defaultrole_desc'] = 'Select role which should be assigned to users during auto enrolment.';
 $string['editenrolment'] = 'Edit enrolment';
@@ -29,14 +34,9 @@ $string['pluginname'] = 'Auto enrolment';
 $string['pluginname_desc'] = 'The auto enrolment plugin automatically enrols users upon course view.';
 $string['privacy:metadata'] = 'The auto enrolment plugin does not store any personal data.';
 $string['role'] = 'Default assigned role';
-$string['auto:config'] = 'Configure auto enrol instances';
-$string['auto:enrolself'] = 'Enrol self in the course';
-$string['auto:manage'] = 'Manage enrolled users';
-$string['auto:unenrol'] = 'Unenrol users from course';
-$string['auto:unenrolself'] = 'Unenrol self from the course';
 $string['status'] = 'Allow auto enrolments';
 $string['status_desc'] = 'Allow auto enrolments of users into course by default.';
 $string['status_help'] = 'This setting determines whether this auto enrol plugin is enabled for this course.';
 $string['unenrol'] = 'Unenrol user';
-$string['unenroluser'] = 'Do you really want to unenrol "{$a->user}" from course "{$a->course}"?';
 $string['unenrolselfconfirm'] = 'Do you really want to unenrol yourself from course "{$a}"?';
+$string['unenroluser'] = 'Do you really want to unenrol "{$a->user}" from course "{$a->course}"?';
